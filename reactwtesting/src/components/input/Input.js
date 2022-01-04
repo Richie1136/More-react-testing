fc897f6e2e865d76164a90c2271fe5c817e67cc5
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-04 10:47:53 -0500

    Wrote react code for the input file

--- reactwtesting/src/components/input/Input.js
+++ reactwtesting/src/components/input/Input.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Input = ({ showDiv }) => {
+  const [searchWord, setSearchWord] = useState('')
+
+  const handleChange = (e) => {
+    setSearchWord(e.target.value)
+  }
   return (
     <div>
-
+      <input
+        type='text'
+        data-testid='searchBar'
+        placeholder='Search...'
+        onChange={handleChange} />
+      <h1 data-testid="displaySearch">{searchWord}</h1>
+      {showDiv && (
+        <div data-testid="divWeWantToShow">
+          Hey My name is Robert
+        </div>
+      )}
     </div>
   )
 }
