076cc492ac4a41ee8dc9657e7f5b6954acc44bb2
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-04 11:20:54 -0500

    Wrote some more tests inside of the input.test file

--- reactwtesting/src/components/input/Input.test.js
+++ reactwtesting/src/components/input/Input.test.js
@@ -1,10 +1,28 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Input from './Input'
 
 describe('Input Component', () => {
   test('rendered input', () => {
     render(<Input showDiv={false} />)
     const input = screen.getByTestId('searchBar')
-    expect(input).toBeTruthy()
+    expect(input).toBeInTheDocument()
+  })
+  test("render div", () => {
+    render(<Input showDiv={true} />)
+    const div = screen.getByTestId('divWeWantToShow')
+    expect(div).toBeInTheDocument()
+  })
+  test("don't render div", () => {
+    render(<Input showDiv={false} />)
+    const div = screen.queryByTestId('divWeWantToShow')
+    expect(div).not.toBeInTheDocument()
+  })
+  test("chnage on input causes change on header", async () => {
+    render(<Input showDiv={true} />)
+    const input = screen.getByTestId('searchBar')
+    const header = screen.getByTestId('displaySearch')
+    const inputWord = "Robert"
+    await fireEvent.change(input, { target: { value: inputWord } })
+    expect(header.innerHTML).toBe(inputWord)
   })
 })
